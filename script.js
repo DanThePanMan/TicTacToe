4f2bae2232d3663ae7dbd4925cbc08d76a5da6d6
Daniel <danielchenschw@gmail.com>
2024-06-06 18:04:33 -0400

    Complete traversal algorithm

--- script.js
+++ script.js
@@ -7,22 +7,48 @@ const gameboard = (function () {
     //use methods in verifyTool to traverse the board
     const verifyTool = checkBoard(board);
 
+    const traverse = (direction, count, type, xIndex, yIndex, board) => {
+        //direction has 8 possibilities 
+        //this is recurseive
+        //l,r,u,d,ul,ur,dl,dr
+
+        //base case
+        if(count === 3){
+            return(type);
+        }
+        else if(board(xIndex, yIndex) != type){
+            return(board(xIndex, yIndex))
+        }
+        else{
+            switch(direction){
+                case "l":
+                    return(traverse("l", count+1, type, xIndex-1, yIndex, board));
+                case "r":
+                    return(traverse("r", count+1, type, xIndex+1, yIndex, board));
+                case "u":
+                    return(traverse("u", count+1, type, xIndex, yIndex+1, board));
+                case "d":
+                    return(traverse("d", count+1, type, xIndex, yIndex-1, board));
+                case "ul":
+                    return(traverse("ul", count+1, type, xIndex-1, yIndex+1, board));
+                case "ur":
+                    return(traverse("ur", count+1, type, xIndex+1, yIndex+1, board));
+                case "dl":
+                    return(traverse("dl", count+1, type, xIndex-1, yIndex-1, board));
+                case "dr":
+                    return(traverse("dr", count+1, type, xIndex+1, yIndex-1, board));
+
+            }   
+        }
+        
+
+    };
+
 
     const verifyWin = () => {
-        for(let i = 0; i < 3; i++){
-            for(let j = 0; j < 3; j++){
-                //this part checks each direciton.
-                //left and right first
-                switch(j){
-                    case 0:
-                        break
-                    case 1:
+        //no loops, just checks each one.
 
-                }
-
-            }
-        }
-
+        
 
     };
     return({ circle, cross, viewBoard, verifyWin })
