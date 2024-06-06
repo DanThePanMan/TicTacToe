2938db33b5e86bf6a27b9c6ecd2f425680b5d00f
Daniel <danielchenschw@gmail.com>
2024-06-05 23:30:45 -0400

    Start Search Algorithm.

--- script.js
+++ script.js
@@ -3,9 +3,31 @@ const gameboard = (function () {
     const circle = (x,y) => {board[x][y] = 'O'};
     const cross = (x,y) => {board[x][y] = 'X'};
     const viewBoard = () => {return board};
-    const verifyWin = () => {};
-    return({ board, circle, cross, viewBoard })
+
+    //use methods in verifyTool to traverse the board
+    const verifyTool = checkBoard(board);
+
+
+    const verifyWin = () => {
+        for(let i = 0; i < 3; i++){
+            for(let j = 0; j < 3; j++){
+                //this part checks each direciton.
+                //left and right first
+                switch(j){
+                    case 0:
+                        break
+                    case 1:
+
+                }
+
+            }
+        }
+
+
+    };
+    return({ circle, cross, viewBoard, verifyWin })
 })();
+
 
 function createPlayer(){
 
